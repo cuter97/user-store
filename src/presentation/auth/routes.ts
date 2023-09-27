90cfe9f20902ff19c0308ab1fc57d7c91953e88f
cuter97 <aguscastets@gmail.com>
2023-09-27 19:15:46 -0300

    create project and connect to mongo

---
+++ src/presentation/auth/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { AuthController } from './controller';
+
+
+
+
+export class AuthRoutes {
+
+
+    static get routes(): Router {
+
+        const router = Router();
+
+        const controller = new AuthController();
+
+        router.post('/login', controller.loginUser);
+        router.post('/register', controller.loginUser);
+        router.get('/validate-email/:token', controller.validateEmail);
+
+        return router;
+    }
+
+
+}
+

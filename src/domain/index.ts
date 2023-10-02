8d1fad27197d5b5edb4f0de235ac55c6511a5495
cuter97 <aguscastets@gmail.com>
2023-10-02 20:41:45 -0300

    create category dto

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,3 +2,5 @@ export * from './errors/custom.errors';
 export * from './entities/user.entity';
 export * from './dtos/auth/register-user.dto';
 export * from './dtos/auth/login-user.dto';
+export * from './dtos/auth/login-user.dto';
+export * from './dtos/category/create-category.dto';
